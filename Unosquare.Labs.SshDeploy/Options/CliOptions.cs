a050412a043b714062e903bae39be3fd7b8cca95
Juan Alcala <juan.alcala@unosquare.com>
2017-11-22 15:59:27 -0600

    Add helptext

--- Unosquare.Labs.SshDeploy/Options/CliOptions.cs
+++ Unosquare.Labs.SshDeploy/Options/CliOptions.cs
@@ -7,16 +7,16 @@ namespace Unosquare.Labs.SshDeploy.Options
 {
     public class CliOptions
     {
-        [VerbOption("push")]
+        [VerbOption("push", HelpText = "Transfers the files and folders from a source path in the local machine to a target path in the remote machine")]
         public PushVerbOptions PushVerbOptions { get; set; }
 
-        [VerbOption("monitor")]
+        [VerbOption("monitor", HelpText = "Monitors a folder for a deployment and automatically transfers the files over the target.")]
         public MonitorVerbOptions MonitorVerbOptions { get; set; }
 
-        [VerbOption("run")]
+        [VerbOption("run", HelpText = "Runs the specified command on the target machine")]
         public RunVerbOptions RunVerbOptions { get; set; }
 
-        [VerbOption("shell")]
+        [VerbOption("shell", HelpText = "Opens an interactive mode shell")]
         public ShellVerbOptions ShellVerbOptions { get; set; }
 
     }
