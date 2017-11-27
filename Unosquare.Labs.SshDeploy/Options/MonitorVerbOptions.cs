2ab3f4a44e2ba5c82a24419ce46a6ab1c032d331
Juan Alcala <juan.alcala@unosquare.com>
2017-11-27 16:21:38 -0600

    Add legacy option

--- Unosquare.Labs.SshDeploy/Options/MonitorVerbOptions.cs
+++ Unosquare.Labs.SshDeploy/Options/MonitorVerbOptions.cs
@@ -8,6 +8,9 @@ namespace Unosquare.Labs.SshDeploy.Options
     public class MonitorVerbOptions : PushVerbOptions
     {
         [ArgumentOption('m', "monitor", DefaultValue = "sshdeploy.ready", HelpText = "The command to run on the target machine", Required = false)]       
-        public string MonitorFile { get; set; } 
+        public string MonitorFile { get; set; }
+
+        [ArgumentOption('l', "legacy", DefaultValue = false, HelpText = "Monitor files using legacy method", Required = false)]
+        public bool Legacy { get; set; }
     }
 }
